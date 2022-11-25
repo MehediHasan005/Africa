15404bc4e4034aa85c7cb8a553ac3e50a837e662
Mehedi Hasan <mehedihasan@Mehedis-MacBook-Air.local>
2022-11-25 19:15:09 +0600

    Africa project start now

---
+++ Africa/Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Mehedi Hasan on 25/11/22.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
