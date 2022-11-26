51215983d566db0420675a54e3446e033619b7c8
MehediHasan005 <mehedihasanhira85@gmail.com>
2022-11-26 10:39:27 +0600

    added video model

---
+++ Africa/Africa/Models/VideoModel.swift
@@ -0,0 +1,15 @@
+//
+//  VideoModel.swift
+//  Africa
+//
+//  Created by Mehedi Hasan on 26/11/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct Video: Codable,Identifiable {
+    let id: String
+    let name: String
+    let headline: String
+}
